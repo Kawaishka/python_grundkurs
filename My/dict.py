ffb83f1a857cc733234b8d3d5d91ca6473932c62
kawaishka <sora.kinl@gmail.com>
2025-10-30 13:49:04 +0100

    Update

--- My/dict.py
+++ My/dict.py
@@ -3,5 +3,16 @@ person = {
   "alter": "26",
   "stadt": "Duesseldorf"
 }
+#Wert ändern
+person["name"] = "Vladyslav"
 
-print(person["name"], person["alter"], person["stadt"])
+#Wert hinzufügen
+person["hobby"] = "tennis"
+person["LVL"] = "20"
+
+print("Name:", person["name"], "Alter:", person["alter"], "Kommt aus:", person["stadt"], person["hobby"], person["LVL"])
+
+# Wert löschen
+
+del person["LVL"]
+print(person)
